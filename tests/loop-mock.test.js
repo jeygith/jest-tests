a14a6258666aff0c7de2749cf958dfe1159e2356
Jeff Githiri <geoffrey.githiri@andela.com>
2020-01-28 19:36:57 +0300

    Add tests for the mock callback

--- tests/loop-mock.test.js
+++ tests/loop-mock.test.js
@@ -1,7 +1,8 @@
 const {forEach} = require('../src/loop-mock');
 describe('#loopMock', () => {
-    beforeAll(() => {
-        const mockCallback = jest.fn(x => 42 + x);
+    const mockCallback = jest.fn(x => 42 + x);
+
+    beforeEach(() => {
     });
 
     it('should call the mocked function twice', () => {
@@ -9,5 +10,30 @@ describe('#loopMock', () => {
         forEach([0, 1], mockCallback);
 
         expect(mockCallback.mock.calls.length).toBe(2);
-    })
+    });
+
+    it('should return 0 as the first argument of the first call to the mock', () => {
+
+        forEach([0, 1], mockCallback);
+
+        expect(mockCallback.mock.calls[0][0]).toBe(0);
+    });
+
+    it('should return 1 as the second argument of the first call to the mock', () => {
+
+        forEach([0, 1], mockCallback);
+
+        expect(mockCallback.mock.calls[1][0]).toBe(1);
+    });
+
+    it('should return the return value of the first call to the mock', () => {
+
+        forEach([0, 1], mockCallback);
+
+        expect(mockCallback.mock.results[0].value).toBe(42);
+    });
+
+
+
+
 });
