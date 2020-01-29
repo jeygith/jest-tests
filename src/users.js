5f39d9d4b2eeb64e4bbdf8ed2d70f09e725c23cc
Jeff Githiri <geoffrey.githiri@andela.com>
2020-01-29 18:11:58 +0300

    add user module and tests

---
+++ src/users.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+class Users {
+    static all() {
+        return axios.get('/users.json')
+            .then(resp => resp.data);
+    }
+}
+
+
+module.exports = {
+    Users
+};
---
+++ tests/users.test.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+const {Users} = require("../src/users");
+
+jest.mock('axios');
+
+it('should fetch users', () => {
+    const users = [{name: 'Jeff'}];
+    const resp = {data: users};
+    axios.get.mockResolvedValue(resp);
+
+    // axios.get.mockImplementation(() => Promise.resolve(resp))
+
+    return Users.all().then(data => expect(data).toEqual(users));
+})
